b7ed6eafd6da04662fb2e8e3f31d5209e2d09cde
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-02-14 13:44:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+
+const App = () => {
+    return (
+        <section className='w-full h-screen overflow-hidden'>
+            <div className='w-full h-full'>
+                <Hero/>
+            </div>
+        </section>
+    )
+}
+
+export default App
